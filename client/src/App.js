02bb101dc96dedb58b3b224fc7bc834deca96b5a
Nathan Huber <Nathan_Huber@yahoo.com>
2020-06-22 13:43:24 -0500

    Added React client, cleaned up default code a bit.  Added a Navbar.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import HammerNavbar from './components/HammerNavbar';
+
+function App() {
+  return (
+    <div className="App">
+     <HammerNavbar />
+    </div>
+  );
+}
+
+export default App;
