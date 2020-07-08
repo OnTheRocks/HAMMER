0cd1e193eeb70751f842cf3f0cbc43e30c99d026
Nathan Huber <Nathan_Huber@yahoo.com>
2020-07-08 16:03:48 -0500

    More updates

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/">HAMMER</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/Tickets">Tickets</NavLink>              
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/Customers">Customers</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/Materials">Materials</NavLink>
+            </NavItem>
+            {/* <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown> */}
+          </Nav>
+          <NavbarText>Nathan Huber</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default NavBar;
