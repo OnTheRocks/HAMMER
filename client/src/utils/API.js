f4c832e396814ef45ca2d978a77a32f83e91ebe2
Nathan Huber <Nathan_Huber@yahoo.com>
2020-11-28 00:09:00 -0600

    Undoing changes that didn't work.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,20 +3,20 @@ import axios from "axios";
 export default { 
    // Gets all tickets
    getTickets: function() {
-     return axios.get("/api/Tickets");
+     return axios.get("/Tickets");
   },
   // Gets the ticket with the given id
   getTicket: function(id) {
-    return axios.get("/api/Tickets/" + id);
+    return axios.get("/Tickets/" + id);
   },
   
   // Deletes the ticket with the given id
   deleteTicket: function(id) {
-    return axios.delete("/api/Tickets/" + id);
+    return axios.delete("/Tickets/" + id);
   },
   // Saves a ticket to the database
   saveTicket: function(ticketData) {
-    return axios.post("/api/Tickets", ticketData);
+    return axios.post("/Tickets", ticketData);
   },
    // Gets all Customers
    getCustomers: function() {
@@ -42,19 +42,19 @@ export default {
   },
  // Gets all Materials
   getMaterials: function() {
-    return axios.get("/api/Materials");
+    return axios.get("/Materials");
   },
   // Gets the Material with the given id
   getMaterial: function(id) {
-    return axios.get("/api/Materials/" + id);
+    return axios.get("/Materials/" + id);
   },
   // Deletes the Material with the given id
   deleteMaterial: function(id) {
-    return axios.delete("/api/materials/" + id);
+    return axios.delete("/materials/" + id);
   },
   // Saves a Material to the database
   saveMaterial: function(materialData) {
-    return axios.post("/api/materials", materialData);
+    return axios.post("/materials", materialData);
   },
 
 };
