85390c85b6ac01217af288f05b17988798030196
Nathan Huber <Nathan_Huber@yahoo.com>
2020-11-30 01:09:09 -0600

    Edited Server.js.

--- server.js
+++ server.js
@@ -18,28 +18,28 @@ app.use(express.json());
 
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 // });
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
+// }
 
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+// else {
+//   app.use(express.static(path.join(__dirname, '/client/public')));
+//   app.get("/*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "./client/public/index.html"));
+//   });
+// }
 
 
 
