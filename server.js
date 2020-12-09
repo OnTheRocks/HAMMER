552a58be730632269f3e6b92b145cbf8f9002860
Nathan Huber <Nathan_Huber@yahoo.com>
2020-12-08 23:50:39 -0600

    Another edit to Server.js to try and get routes in heroku to work.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/*', (req, res) => {
   let url = path.join(__dirname, '../client/build', 'index.html');
   if (!url.startsWith('/app/')) // we're on local windows
     url = url.substring(1);
-  res.sendFile(path.join(__dirname, '/client/public/index.html'));
+  res.sendFile(url);
 });
 
 // app.get('*', (req, res) => {
