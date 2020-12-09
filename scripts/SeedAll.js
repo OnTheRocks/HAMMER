e9e7a3c4725d0675a8ec6dbeb3a0e9aec8686908
Nathan Huber <Nathan_Huber@yahoo.com>
2020-12-09 02:02:19 -0600

    FINALLY GOT THE ROUTES WORKING!!!!!!  Also fixed up the seed data a bit.

--- scripts/SeedAll.js
+++ scripts/SeedAll.js
@@ -113,48 +113,52 @@ Customer
     });
 
   const ticketSeed = [
-    {       
-      "ticketDate": "7/4/2020",
-      "ticketNum": 12345,
-      "ticketCust": "Concrete Industries",
-      "ticketTareWeight": 35000,
-      "ticketGrossWeight": 85000,
-      "ticketNetWeight": 50000
-    },
-    {       
-      "ticketDate": "10/1/2020",
-      "ticketNum": 12346,
-      "ticketCust": "Dick Construction Inc.",
-      "ticketTareWeight": 35000,
-      "ticketGrossWeight": 85000,
-      "ticketNetWeight": 50000
-    },
-    {       
-      "ticketDate": "10/2/2020",
-      "ticketNum": 12347,
-      "ticketCust": "Ozinga",
-      "ticketTareWeight": 35000,
-      "ticketGrossWeight": 85000,
-      "ticketNetWeight": 50000
-    },
-    {       
-      "ticketDate": "10/5/2020",
-      "ticketNum": 12348,
-      "ticketCust": "Lee Construction Inc.",
-      "ticketTareWeight": 35000,
-      "ticketGrossWeight": 85000,
-      "ticketNetWeight": 50000
-    },
-    {       
-      "ticketDate": "10/10/2020",
-      "ticketNum": 12349,
-      "ticketCust": "Dunlap Construction",
-      "ticketTareWeight": 35000,
-      "ticketGrossWeight": 85000,
-      "ticketNetWeight": 50000
-    },
-    
-  ];
+  {       
+    "ticketDate": "7/4/2020",
+    "ticketNum": 12345,
+    "ticketCust": "Concrete Industries",
+    "ticketMaterial": "Mason Sand",
+    "ticketTareWeight": 35000,
+    "ticketGrossWeight": 85000,
+    "ticketNetWeight": 50000
+  },
+  {       
+    "ticketDate": "10/1/2020",
+    "ticketNum": 12346,
+    "ticketCust": "Dick Construction Inc.",
+    "ticketMaterial": "Ice Control Sand",
+    "ticketTareWeight": 35000,
+    "ticketGrossWeight": 85000,
+    "ticketNetWeight": 50000
+  },
+  {       
+    "ticketDate": "10/2/2020",
+    "ticketNum": 12347,
+    "ticketCust": "Ozinga",
+    "ticketMaterial": "Oversized Rock",
+    "ticketTareWeight": 35000,
+    "ticketGrossWeight": 85000,
+    "ticketNetWeight": 50000
+  },
+  {       
+    "ticketDate": "10/5/2020",
+    "ticketNum": 12348,
+    "ticketCust": "Lee Construction Inc.",
+    "ticketMaterial": "Fill Sand",
+    "ticketTareWeight": 35000,
+    "ticketGrossWeight": 85000,
+    "ticketNetWeight": 50000
+  },
+  {       
+    "ticketDate": "10/10/2020",
+    "ticketNum": 12349,
+    "ticketCust": "Dunlap Construction",
+    "ticketMaterial": "3/4 Crushed Rock",
+    "ticketTareWeight": 35000,
+    "ticketGrossWeight": 85000,
+    "ticketNetWeight": 50000
+  },  
+];
   
   Ticket
     Ticket.deleteMany({})
--- scripts/TicketDBSeed.js
+++ scripts/TicketDBSeed.js
@@ -13,6 +13,7 @@ const ticketSeed = [
     "ticketDate": "7/4/2020",
     "ticketNum": 12345,
     "ticketCust": "Concrete Industries",
+    "ticketMaterial": "Mason Sand",
     "ticketTareWeight": 35000,
     "ticketGrossWeight": 85000,
     "ticketNetWeight": 50000
@@ -21,6 +22,7 @@ const ticketSeed = [
     "ticketDate": "10/1/2020",
     "ticketNum": 12346,
     "ticketCust": "Dick Construction Inc.",
+    "ticketMaterial": "Ice Control Sand",
     "ticketTareWeight": 35000,
     "ticketGrossWeight": 85000,
     "ticketNetWeight": 50000
@@ -29,6 +31,7 @@ const ticketSeed = [
     "ticketDate": "10/2/2020",
     "ticketNum": 12347,
     "ticketCust": "Ozinga",
+    "ticketMaterial": "Oversized Rock",
     "ticketTareWeight": 35000,
     "ticketGrossWeight": 85000,
     "ticketNetWeight": 50000
@@ -37,6 +40,7 @@ const ticketSeed = [
     "ticketDate": "10/5/2020",
     "ticketNum": 12348,
     "ticketCust": "Lee Construction Inc.",
+    "ticketMaterial": "Fill Sand",
     "ticketTareWeight": 35000,
     "ticketGrossWeight": 85000,
     "ticketNetWeight": 50000
@@ -45,11 +49,11 @@ const ticketSeed = [
     "ticketDate": "10/10/2020",
     "ticketNum": 12349,
     "ticketCust": "Dunlap Construction",
+    "ticketMaterial": "3/4 Crushed Rock",
     "ticketTareWeight": 35000,
     "ticketGrossWeight": 85000,
     "ticketNetWeight": 50000
-  },
-  
+  },  
 ];
 
 Ticket
