7b63e96858bda46251571e1d20c781b8afec5212
Nathan Huber <Nathan_Huber@yahoo.com>
2020-12-09 01:30:43 -0600

    Applying corrections to the Tickets and Materials Routes.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,20 +3,20 @@ import axios from "axios";
 export default { 
    // Gets all tickets
    getTickets: function() {
-     return axios.get("/Tickets");
+     return axios.get("/api/Tickets");
   },
   // Gets the ticket with the given id
   getTicket: function(id) {
-    return axios.get("/Tickets/" + id);
+    return axios.get("/api/Tickets/" + id);
   },
   
   // Deletes the ticket with the given id
   deleteTicket: function(id) {
-    return axios.delete("/Tickets/" + id);
+    return axios.delete("/api/Tickets/" + id);
   },
   // Saves a ticket to the database
   saveTicket: function(ticketData) {
-    return axios.post("/Tickets", ticketData);
+    return axios.post("/api/Tickets", ticketData);
   },
    // Gets all Customers
    getCustomers: function() {
@@ -24,37 +24,37 @@ export default {
   },
   // Gets the customer with the given id
   getCustomer: function(id) {
-    return axios.get("/Customers/" + id);
+    return axios.get("/api/Customers/" + id);
   }, 
 
   getCustLocations: function(id) {
-    return axios.get("/Customers/" + id + "/locations");
+    return axios.get("/api/Customers/" + id + "/locations");
   },
   
   
   // Deletes the customer with the given id
   deleteCustomer: function(id) {
-    return axios.delete("/Customers/" + id);
+    return axios.delete("/api/Customers/" + id);
   },
   // Saves a customer to the database
   saveCustomer: function(customerData) {
-    return axios.post("/Customers", customerData);
+    return axios.post("/api/Customers", customerData);
   },
  // Gets all Materials
   getMaterials: function() {
-    return axios.get("/Materials");
+    return axios.get("/api/Materials");
   },
   // Gets the Material with the given id
   getMaterial: function(id) {
-    return axios.get("/Materials/" + id);
+    return axios.get("/api/Materials/" + id);
   },
   // Deletes the Material with the given id
   deleteMaterial: function(id) {
-    return axios.delete("/materials/" + id);
+    return axios.delete("/api/materials/" + id);
   },
   // Saves a Material to the database
   saveMaterial: function(materialData) {
-    return axios.post("/materials", materialData);
+    return axios.post("/api/materials", materialData);
   },
 
 };
