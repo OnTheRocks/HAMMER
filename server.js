7fdac290766298e325000e8e4b9817556392a1cf
Nathan Huber <Nathan_Huber@yahoo.com>
2020-12-29 23:55:08 -0600

    Edited server.  Working on Passport Authentication.

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require("./routes");
 const app = express();
-// const passport = require('passport');
-// const path = require('path')
+const path = require('path')
+
+
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override')
+
+
+
 
 const port = process.env.PORT || 3030;
 
@@ -13,16 +22,83 @@ const customers = require('./routes/api/Customers');
 const materials = require('./routes/api/Materials');
 const tickets = require('./routes/api/Tickets');
 
-// const initalizePassport = require('./passport.config');
-// initalizePassport(
-//   passport,
-//   email => users.find(user => user.email === email), 
-//   id => users.find(user => user.id === id)
-// )
+const initalizePassport = require('./passport.config');
+initalizePassport(
+  passport,
+  email => users.find(user => user.email === email), 
+  id => users.find(user => user.id === id)
+)
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(flash());
+app.use(session( {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name})
+})
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+  console.log(users);
+})
+
+app.delete('/logOut', (req, res) => {
+  req.logOut()
+  res.redirect('./login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/') 
+  }
+  next()
+}
+
+
+
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
