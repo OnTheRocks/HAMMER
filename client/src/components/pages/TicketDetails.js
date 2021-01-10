2ffa9dfc2274b9daddc17f3915f8b69ceee3e004
Nathan Huber <Nathan_Huber@yahoo.com>
2021-01-10 00:40:18 -0600

    Cleaned TicketDetail code a bit.

--- client/src/components/pages/TicketDetails.js
+++ client/src/components/pages/TicketDetails.js
@@ -8,8 +8,6 @@ const moment = require('moment');
 
 function TicketDetails(props) {
   const [tickets, setTickets] = useState({})
-  
-
 
   // When this component mounts, grab the ticket with the _id of props.match.params.id
   // e.g. localhost:3000/Tickets/599dcb67f0f16317844583fc
